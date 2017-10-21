0db16d8953c513518e5431c7966df83db0d2bdb0
mtrimbur <marius.trimbur@gmail.com>
2017-10-21 18:41:14 +0200

    Added pokemon's types

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,12 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {ShadowCardDirective} from './shadow-card.directive';
+import { PokemonTypeColorPipe } from './pokemon-type-color.pipe';
 
 
 @NgModule({
     imports: [BrowserModule],
-    declarations: [AppComponent, ShadowCardDirective],
+    declarations: [AppComponent, ShadowCardDirective, PokemonTypeColorPipe],
     bootstrap: [AppComponent]
 })
 export class AppModule {}
